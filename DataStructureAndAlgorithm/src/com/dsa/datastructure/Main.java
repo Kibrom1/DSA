c299b7b422384a338151f68657b99a6dad90df9b
Kibrom1 <kibmit@gmail.com>
2019-10-27 10:33:18 -0400

    First commit of DSA

---
+++ DataStructureAndAlgorithm/src/com/dsa/datastructure/Main.java
@@ -0,0 +1,9 @@
+package com.dsa.datastructure;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here this will be used to test all our
+        System.out.println("THis is the first trial in the data structure course");
+    }
+}
