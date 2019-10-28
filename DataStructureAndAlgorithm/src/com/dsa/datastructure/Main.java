8e401f758ea27af3f2cb267a567c57f3059460ee
Kibrom1 <kibmit@gmail.com>
2019-10-28 17:51:47 -0400

    Recursion completed

--- DataStructureAndAlgorithm/src/com/dsa/datastructure/Main.java
+++ DataStructureAndAlgorithm/src/com/dsa/datastructure/Main.java
@@ -1,10 +1,21 @@
 package com.dsa.datastructure;
 
+import com.dsa.datastructure.recurssion.RecursionStack;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here this will be used to test all our
-        System.out.println("THis is the first trial in the data structure course");
+	    //Testing how stack works when executing a function
+        RecursionStack.mainPrinter();
+
+        //Testing recursive call and stack together
+        RecursionStack.foo(3);
+
+        //Testing recursion with factorial
+        System.out.println(RecursionStack.factorial(5));
+
+        //Testing recursion using fibonacci series
+        System.out.println(RecursionStack.fibonacci(6));
     }
 }
     
